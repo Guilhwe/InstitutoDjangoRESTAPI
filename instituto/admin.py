9701ee9c4110899cc77780be981396a63ffe28fc
Guilherme Melo <guilherme96melo@gmail.com>
2024-08-27 20:37:12 +0200

    creacion del admin y de 2 estudiantes de ejemplo y un curso. todavia el database no esta siendo accesado por las views

--- instituto/admin.py
+++ instituto/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from instituto.models import Estudiante,Curso
 
-# Register your models here.
+class Estudiantes(admin.ModelAdmin):
+    list_display = ('id','nombre', 'email','dni','fecha_nacimiento','movil')
+    list_display_links=('id','nombre',)
+    list_per_page =20
+    search_fields=('nombre',)
+
+admin.site.register(Estudiante, Estudiantes)
+
+class Cursos(admin.ModelAdmin):
+    list_display = ('id','codigo','descripcion')
+    list_display_links=('id','codigo',)
+    search_fields=('codigo',)
+
+admin.site.register(Curso,Cursos)
