d52f892a6fa2102e041d96b4cdae0c3ef09587e6
Guilherme Melo <guilherme96melo@gmail.com>
2024-08-27 18:24:44 +0200

    creacion de las primeras clases en models de donde sacara la base de datos las infos necesarias

--- instituto/models.py
+++ instituto/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Estudiante(models.Model):
+    nombre=models.CharField(max_lenght = 50)
+    email =models.EmailField(blank=False, max_length=30)
+    dni=models.CharField(max_length=11)
+    fecha_nacimiento=models.DateField
+    movil=models.CharField(max_lenght = 9)
+
+    def __str__(self):
+        return self.nombre
+class Curso(models.Model)
