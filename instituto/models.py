146dec642ffa6af204cd07d70e0112e5a1cbda11
Guilherme Melo <guilherme96melo@gmail.com>
2024-08-28 19:08:20 +0200

    cambios en models con sus migraciones correspondientes. Uso del  ForeignKey para usar un metodo many to one. aplicacion  del many to one a la matricula que es el one y estudiante yu curso el many

--- instituto/models.py
+++ instituto/models.py
@@ -21,3 +21,13 @@ class Curso(models.Model):
     
     def __str__(self):
         return self.codigo
+    
+class Matricula(models.Model):
+    TURNO = (
+        ('M','Mañana'),
+        ('T','Tarde'),
+        ('N','Noche'),
+    )
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    curso =  models.ForeignKey(Curso, on_delete=models.CASCADE)
+    turno = models.CharField(max_length= 1, choices= TURNO, blank =False, null=False, default='M' )
