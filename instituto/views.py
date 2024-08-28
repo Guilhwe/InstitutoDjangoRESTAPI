fbdff644fe6b07d502f98977ac453802b9fa13e4
Guilherme Melo <guilherme96melo@gmail.com>
2024-08-28 19:41:05 +0200

    Mismo tramite para curso

--- instituto/views.py
+++ instituto/views.py
@@ -1,6 +1,6 @@
 from instituto.models import Estudiante,Curso,Matricula
-from instituto.serializers import EstudianteSerializer,CursoSerializer,MatriculaSerializer
-from rest_framework import viewsets
+from instituto.serializers import EstudianteSerializer,CursoSerializer,MatriculaSerializer, ListaMatriculasCursoSerializer,ListaMatriculasEstudianteSerializer
+from rest_framework import viewsets,generics
 
 class EstudianteViewSet(viewsets.ModelViewSet):
     queryset = Estudiante.objects.all()
@@ -13,3 +13,15 @@ class CursoViewSet(viewsets.ModelViewSet):
 class MatriculaViewSet(viewsets.ModelViewSet):
     queryset= Matricula.objects.all()
     serializer_class = MatriculaSerializer
+
+class ListaMatriculaEstudiante(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(estudiante_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasEstudianteSerializer
+        
+class ListaMatriculaCurso(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasCursoSerializer
