eeaf035b888ed489fa9dd5a09218749c65614bf1
Guilherme Melo <guilherme96melo@gmail.com>
2024-09-02 21:36:11 +0200

    CREACION del archivo de throttle para controlar las peticiones de acceso diarias a rutas especificas y no global como se hizo en settings. modificaciones pertinentes en views

--- instituto/views.py
+++ instituto/views.py
@@ -2,6 +2,8 @@ from instituto.models import Estudiante,Curso,Matricula
 from instituto.serializers import EstudianteSerializer,CursoSerializer,MatriculaSerializer, ListaMatriculasCursoSerializer,ListaMatriculasEstudianteSerializer,EstudianteSerializerV2
 from rest_framework import viewsets,generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.throttling import UserRateThrottle
+from instituto.throttle import MatriculaAnonRateThrottle
 #PARA QUE LA AUTENTIFIACION NO SEA GENERAL HAY QUE IMPORTAR EN VIEWS DESDE RESTFRAMEWORK LAS ATENTIFICACIONES Y PERMISIONES MY PONERLAS EN CADA VIEWS
 
 class EstudianteViewSet(viewsets.ModelViewSet):
@@ -21,10 +23,12 @@ class CursoViewSet(viewsets.ModelViewSet):
     queryset= Curso.objects.all().order_by('id')
     serializer_class = CursoSerializer
 
+
 class MatriculaViewSet(viewsets.ModelViewSet):
     
     queryset= Matricula.objects.all().order_by('id')
     serializer_class = MatriculaSerializer
+    throttle_classes =[UserRateThrottle, MatriculaAnonRateThrottle, ]
 
 class ListaMatriculaEstudiante(generics.ListAPIView):
     
