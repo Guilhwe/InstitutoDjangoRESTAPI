8cabe8447c2f2add4b771a516118f68ef8185767
Guilherme Melo <guilherme96melo@gmail.com>
2024-09-02 19:19:02 +0200

    modificacion del validator de dni no se ha importado el validatror desde el paquete docBR pues se están haciendo las matriculas con el DNI español pero es posible importar un paquete de validaciones con PIP y utilizarlos

--- instituto/validators.py
+++ instituto/validators.py
@@ -1,6 +1,8 @@
 import re
 def dni_invalido(dni):
-        return len(dni) != 11
+        modelo = '[0-9]{10}[a-z]{1}'
+        respuesta = re.findall(modelo,dni)
+        return not respuesta
 def nombre_invalido(nombre):
         return not nombre.isalpha()
 def movil_invalido(movil):
