61d37ab8d3e8444611f68c3fd558cd9303ac86f8
Guilherme Melo <guilherme96melo@gmail.com>
2024-09-02 19:12:32 +0200

    importacion de validacionesdel RE usando un modelo en validators

--- instituto/validators.py
+++ instituto/validators.py
@@ -1,9 +1,12 @@
+import re
 def dni_invalido(dni):
-        return len(dni) != 1
+        return len(dni) != 11
 def nombre_invalido(nombre):
         return not nombre.isalpha()
 def movil_invalido(movil):
-        return len(movil) != 9
+        modelo = '[0-9]{9}'
+        respuesta = re.findall(modelo,movil)
+        return not respuesta
 
 
 
