f125ee5aed62d45cd54bd2d3d8139f9cb6d25dfb
Guilherme Melo <guilherme96melo@gmail.com>
2024-09-03 19:24:57 +0200

    modificacion de permisos  en admin de usuarios de ejemplo y modificacion del settings para que los permisos no sean solamente por estar autentificados

--- setup/settings.py
+++ setup/settings.py
@@ -130,12 +130,12 @@ REST_FRAMEWORK = {
          'rest_framework.authentication.BasicAuthentication'
         ],
      'DEFAULT_PERMISSION_CLASSES': [
-         'rest_framework.permissions.IsAuthenticated'
+         'rest_framework.permissions.DjangoModelPermissions',
          ],
         'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
          'PAGE_SIZE': 10,
          'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
-             'DEFAULT_THROTTLE_CLASSES': [
+         'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle'
         ],
