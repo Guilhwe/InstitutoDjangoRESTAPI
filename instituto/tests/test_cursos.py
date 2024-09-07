6bdf95b8db62adc108a4c59ce7c94effa13c0641
Guilherme Melo <guilherme96melo@gmail.com>
2024-09-07 22:14:58 +0200

    pruebas extras

--- instituto/tests/test_cursos.py
+++ instituto/tests/test_cursos.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
+from instituto.models import Curso
+from instituto.serializers import CursoSerializer
 
 
 class CursosTestCase(APITestCase):
@@ -9,3 +11,20 @@ class CursosTestCase(APITestCase):
         self.usuario = User.objects.create_superuser(username='admin',password='admin')
         self.url = reverse('Curso-list')
         self.client.force_authenticate(user=self.usuario)
+
+    def test_requisicion_get_para_listar_cursos(self):
+        '''Test de requisicion GET'''
+        response = self.client.get(self.url)#/cursos/
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_requisicion_post_para_crear_un_curso(self):
+        '''Test de requisicion POST para un curso'''
+        datos={
+            'nivel':'B',
+            'codigo':'test',
+            'descripcion':'descripcion',
+           
+        }
+        response = self.client.post(self.url,datos)
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+        
--- instituto/tests/test_matriculas.py
+++ instituto/tests/test_matriculas.py
@@ -2,6 +2,9 @@ from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
+from instituto.models import Matricula
+from instituto.serializers import MatriculaSerializer
+
 
 
 class MatriculasTestCase(APITestCase):
@@ -9,3 +12,5 @@ class MatriculasTestCase(APITestCase):
         self.usuario = User.objects.create_superuser(username='admin',password='admin')
         self.url = reverse('Matricula-list')
         self.client.force_authenticate(user=self.usuario)
+    
+    
